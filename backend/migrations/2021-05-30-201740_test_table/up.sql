8b2f55b42a47a7d8e40a722e94f3b4496b843c60
nilSinger <niilz@outlook.de>
2021-05-31 21:37:09 +0200

    Added necessary files and code to compile the 'embed-step' in diesel

---
+++ backend/migrations/2021-05-30-201740_test_table/up.sql
@@ -0,0 +1,5 @@
+CREATE TABLE test_table (
+	is SERIAL PRIMARY KEY,
+	name VARCHAR NOT NULL,
+	description VARCHAR,
+);
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -2,11 +2,33 @@
 #[macro_use]
 extern crate rocket;
 
+#[macro_use]
+extern crate diesel;
+
+#[macro_use]
+extern crate diesel_migrations;
+
+use diesel::pg::PgConnection;
+use diesel::prelude::*;
+use dotenv::dotenv;
+use std::env;
+
+diesel_migrations::embed_migrations!();
+
 #[get("/")]
 fn hello() -> &'static str {
     "Hello World from Rocket"
 }
 
 fn main() {
+    dotenv().ok();
+    let connection = establish_connection();
+    embedded_migrations::run(&connection);
+
     rocket::ignite().mount("/", routes![hello]).launch();
+}
+
+fn establish_connection() -> PgConnection {
+    let db_url = env::var("DATABASE_URL").expect("Could not read DATABASE_URL from env");
+    PgConnection::establish(&db_url).expect("Could not establish_connection()")
 }
