2062fcd63481e785dfb4ab8395c1f7386e01fff1
nilSinger <niilz@outlook.de>
2021-06-03 22:06:23 +0200

    Modified the fetch call to send some dummy-login-details

--- frontend/src/services/auth-service.tsx
+++ frontend/src/services/auth-service.tsx
@@ -1,19 +1,27 @@
 const authApi = 'http://localhost:8000/';
 
 const baseHeaders = new Headers();
-baseHeaders.set('Accept', 'text/plain');
+baseHeaders.set('Accept', 'application/json');
+baseHeaders.set('Content-Type', 'application/json');
+
+const user = {
+  user_name: 'test user',
+  pwd: 'mega-secret',
+};
 
 const baseOptions: RequestInit = {
-  method: 'GET',
+  method: 'POST',
   headers: baseHeaders,
   mode: 'cors',
   cache: 'default',
+  body: JSON.stringify(user),
 };
 
 class AuthService {
   static checkPassword(pwd: String) {
-    return fetch(authApi, baseOptions)
-      .then((xx) => console.log(xx))
+    return fetch(`${authApi}login`, baseOptions)
+      .then((body) => body.text())
+      .then((json) => console.log(json))
       .then(() => true);
   }
 }
