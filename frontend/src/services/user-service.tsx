ce8a40ccc3fc1e0e116d5923a67809f0d35f7ec3
nilSinger <niilz@outlook.de>
2021-06-12 07:39:57 +0200

    Added two fetch-request-functions: - deleteUser - getAllUsers Note: Not yet compatible with backend

--- frontend/src/services/user-service.tsx
+++ frontend/src/services/user-service.tsx
@@ -5,3 +5,11 @@ import { fetchWrapper } from './fetch-service';
 export function insertUser(user: User) {
   return fetchWrapper(METHOD.POST, 'register', JSON.stringify(user));
 }
+
+export function deleteUser(id: Number) {
+  return fetchWrapper(METHOD.POST, `delete${id}`, '');
+}
+
+export function getAllUsers() {
+  return fetchWrapper(METHOD.GET, 'all', '');
+}
