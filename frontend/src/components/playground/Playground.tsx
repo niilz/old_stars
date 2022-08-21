e23b4ae425dcf639115e2f20eeee46fc4faac2fa
nilSinger <niilz@outlook.de>
2022-08-21 15:58:15 +0200

    Add WaterRounndWarning component

--- frontend/src/components/playground/Playground.tsx
+++ frontend/src/components/playground/Playground.tsx
@@ -5,6 +5,7 @@ import { UserList } from '../user-list/UserList';
 import { Button } from '../button/Button';
 import { UserView } from '../user/UserView';
 import { Modal } from '../modal/Modal';
+import { WaterRoundWarning } from '../waterround-warning/WaterRoundWarning';
 
 interface PlaygroundProps {
   logout: () => void;
@@ -19,9 +20,7 @@ export function Playground(props: PlaygroundProps) {
   return (
     <div className={styles.Playground}>
       <Header showLogo={true} />
-      {oldstar.needsWaterRound() && (
-        <Modal children={<div>Du musst eine Wasserrunde trinken</div>} />
-      )}
+      {oldstar.needsWaterRound() && <Modal children={<WaterRoundWarning/>} />}
       <UserView user={props.user} onUserUpdate={props.onUserUpdate} />
       <UserList users={props.users} isEditable={false} />
       <div className={styles.buttons}>
---
+++ frontend/src/components/waterround-warning/WaterRoundWarning.tsx
@@ -0,0 +1,4 @@
+import styles from './WaterRoundWarning.module.css';
+export function WaterRoundWarning() {
+  return <p className={styles.warning}>Du musst eine Wasserrunde trinken</p>;
+}
