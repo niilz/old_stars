53d39407cb711a6eecd92f4523732a629f0713a9
nilSinger <niilz@outlook.de>
2022-10-09 15:16:20 +0200

    Remove unused TlsConfig-helper

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -1,14 +1,6 @@
 #[macro_use]
 extern crate diesel;
 
-use rocket::config::{CipherSuite, TlsConfig};
-
 pub mod db;
 pub mod model;
 pub mod schema;
-
-pub fn tls_config(cert_path: &str, privat_key: &str) -> TlsConfig {
-    TlsConfig::from_paths(cert_path, privat_key)
-        .with_ciphers(CipherSuite::TLS_V13_SET)
-        .with_preferred_server_cipher_order(true)
-}
