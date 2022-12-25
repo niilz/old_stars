eeedd06f14178f5ed42750d6ff71f30b00655baf
nilSinger <niilz@outlook.de>
2022-12-25 14:31:40 +0100

    Move the functions for dealing with user into DbUserService as methods

--- backend/src/db/user_service.rs
+++ backend/src/db/user_service.rs
@@ -9,21 +9,77 @@ use diesel::{insert_into, prelude::*, PgConnection};
 use std::error::Error;
 use std::fmt;
 
+use super::connection::OldStarDb;
+
 #[derive(Debug)]
 pub struct UserServiceError {
     message: String,
 }
 
 pub struct DbUserService {
-    pub conn: PgConnection,
+    pub db: OldStarDb,
 }
 
 impl UserService for DbUserService {
     fn get_user_by_name(&self, user_name: &str) -> Result<User, UserServiceError> {
         let user = old_users
             .filter(name.eq(user_name))
-            .first::<User>(&self.conn)?;
+            .first::<User>(&self.db.conntection())?;
         Ok(user)
+    }
+
+    fn insert_user(&self, new_user: LoginData) -> Result<User, UserServiceError> {
+        // Do not allow for duplicate users
+        let users_with_given_name = old_users
+            .filter(name.eq(&new_user.name))
+            .load::<User>(&self.db.conntection())?;
+
+        if users_with_given_name.len() != 0 {
+            return Err(UserServiceError::new(
+                "Registration",
+                &"User already exists",
+            ));
+        }
+        let hashed_pwd = hash(&new_user.pwd)?;
+        let inserted_user = insert_into(old_users)
+            .values((
+                name.eq(new_user.name),
+                pwd.eq(hashed_pwd.to_string()),
+                beer_count.eq(0),
+                shot_count.eq(0),
+                water_count.eq(0),
+                fk_icon_id.eq(42),
+            ))
+            .get_result(&self.db.conntection())?;
+        Ok(inserted_user)
+    }
+
+    fn delete_user(&self, del_id: i32) -> Result<User, UserServiceError> {
+        let deleted_user =
+            diesel::delete(old_users.filter(id.eq(del_id))).get_result(&self.db.conntection())?;
+        Ok(deleted_user)
+    }
+
+    fn add_drink_to_user<'a>(
+        &self,
+        update_id: i32,
+        drink: &'a str,
+    ) -> Result<User, UserServiceError> {
+        let update_user = old_users.filter(id.eq(update_id));
+        let connection = self.db.conntection();
+        let updated_user = match drink {
+            "beer" => diesel::update(update_user)
+                .set(beer_count.eq(beer_count + 1))
+                .get_result(&connection)?,
+            "shot" => diesel::update(update_user)
+                .set(shot_count.eq(shot_count + 1))
+                .get_result(&connection)?,
+            "water" => diesel::update(update_user)
+                .set(water_count.eq(water_count + 1))
+                .get_result(&connection)?,
+            _ => unimplemented!("Other drinks are not supported"),
+        };
+        Ok(updated_user)
     }
 }
 
@@ -54,59 +110,12 @@ impl From<password_hash::Error> for UserServiceError {
     }
 }
 
-pub fn insert_user(conn: &PgConnection, user: LoginData) -> Result<User, UserServiceError> {
-    // Do not allow for duplicate users
-    let users_with_given_name = old_users.filter(name.eq(&user.name)).load::<User>(conn)?;
-
-    if users_with_given_name.len() != 0 {
-        return Err(UserServiceError::new(
-            "Registration",
-            &"User already exists",
-        ));
-    }
-    let hashed_pwd = hash(&user.pwd)?;
-    let inserted_user = insert_into(old_users)
-        .values((
-            name.eq(user.name),
-            pwd.eq(hashed_pwd.to_string()),
-            beer_count.eq(0),
-            shot_count.eq(0),
-            water_count.eq(0),
-            fk_icon_id.eq(42),
-        ))
-        .get_result(conn)?;
-    Ok(inserted_user)
-}
-
 pub fn get_users(conn: &PgConnection) -> QueryResult<Vec<User>> {
     // TODO: Make seperate types or tables instead of _secret_ users-names
+    println!("calling get_users");
     old_users
         .filter(not(name.eq("club").or(name.eq("admin!"))))
         .load::<User>(conn)
-}
-
-pub fn delete_user_from_db(conn: &PgConnection, del_id: i32) -> QueryResult<User> {
-    diesel::delete(old_users.filter(id.eq(del_id))).get_result(conn)
-}
-
-pub fn add_drink_to_user<'a>(
-    conn: &PgConnection,
-    update_id: i32,
-    drink: &'a str,
-) -> QueryResult<User> {
-    let update_user = old_users.filter(id.eq(update_id));
-    match drink {
-        "beer" => diesel::update(update_user)
-            .set(beer_count.eq(beer_count + 1))
-            .get_result(conn),
-        "shot" => diesel::update(update_user)
-            .set(shot_count.eq(shot_count + 1))
-            .get_result(conn),
-        "water" => diesel::update(update_user)
-            .set(water_count.eq(water_count + 1))
-            .get_result(conn),
-        _ => unimplemented!("Other drinks are not supported"),
-    }
 }
 
 #[cfg(test)]
