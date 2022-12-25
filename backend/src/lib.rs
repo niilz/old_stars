e5b4390eba6cf2cf49575647f97279d79b83d04f
nilSinger <niilz@outlook.de>
2022-12-25 14:32:40 +0100

    Define insert_, delete_, add_drink_to_user to UserService-Trait

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -6,8 +6,15 @@ pub mod model;
 pub mod schema;
 
 use db::user_service::UserServiceError;
-use model::user::User;
+use model::{login_data::LoginData, user::User};
 
-pub trait UserService {
+pub trait UserService: Send + Sync {
     fn get_user_by_name(&self, user_name: &str) -> Result<User, UserServiceError>;
+    fn insert_user(&self, new_user: LoginData) -> Result<User, UserServiceError>;
+    fn delete_user(&self, id: i32) -> Result<User, UserServiceError>;
+    fn add_drink_to_user<'a>(
+        &self,
+        update_id: i32,
+        drink: &'a str,
+    ) -> Result<User, UserServiceError>;
 }
