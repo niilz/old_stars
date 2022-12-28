c78d11d868bc7dc06936ab75206841570a6589b0
nilSinger <niilz@outlook.de>
2022-12-28 13:41:56 +0100

    Remove obsolete comments and add expiration to session

--- backend/src/model/session.rs
+++ backend/src/model/session.rs
@@ -1,18 +1,13 @@
-// use crate::model::user::User;
-// use crate::schema::sessions;
-// use diesel::{Associations, Identifiable, Queryable};
-// use serde::Deserialize;
+use std::time::{Duration, Instant};
 use uuid::Uuid;
 
-//#[derive(Deserialize, Queryable, Identifiable, Associations)]
-//#[belongs_to(User)]
-//#[table_name = "sessions"]
+const TWENTY_FOUR_HOURS: u64 = 60 * 60 * 24;
+
 pub struct Session {
     //id: i32,
     user: String,
     uuid: String,
-    //value: String,
-    //user_id: i32,
+    exp: Instant,
 }
 
 impl Session {
@@ -20,6 +15,7 @@ impl Session {
         Self {
             user: user_name.to_string(),
             uuid: Uuid::new_v4().to_string(),
+            exp: Instant::now() + Duration::from_secs(TWENTY_FOUR_HOURS),
         }
     }
 }
@@ -35,5 +31,11 @@ mod test {
 
         assert_eq!(dummy_user.to_string(), session.user);
         assert_eq!(36, session.uuid.len());
+        let tomorrow = Instant::now() + Duration::from_secs(TWENTY_FOUR_HOURS);
+        let one_minute = Duration::from_secs(60);
+        let one_minute_earlier = tomorrow - one_minute;
+        let one_minute_later = tomorrow + one_minute;
+        assert!(session.exp > one_minute_earlier);
+        assert!(session.exp < one_minute_later);
     }
 }
