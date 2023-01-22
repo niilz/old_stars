230d639aea4dd68cce17cb54b9214bcd494197a6
nilSinger <niilz@outlook.de>
2023-01-22 18:11:06 +0100

    Add SessionResponse struct to lib

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -1,3 +1,6 @@
+use model::app_user::AppUser;
+use serde::Serialize;
+
 #[macro_use]
 extern crate diesel;
 
@@ -5,3 +8,16 @@ pub mod model;
 pub mod repository;
 pub mod schema;
 pub mod service;
+
+#[derive(Debug, PartialEq, Eq, Serialize)]
+pub struct SessionResponse {
+    user: AppUser,
+    #[serde(rename = "sessionId")]
+    session_id: String,
+}
+
+impl SessionResponse {
+    pub fn new(user: AppUser, session_id: String) -> Self {
+        Self { user, session_id }
+    }
+}
