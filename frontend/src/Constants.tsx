221fdfebf5e004e6f71d7d38ed87047258541d73
nilSinger <niilz@outlook.de>
2023-01-22 18:18:35 +0100

    Add constant for session-token-header

--- frontend/src/Constants.tsx
+++ frontend/src/Constants.tsx
@@ -1,6 +1,8 @@
 export const API_URL_DEV = 'http://localhost:8000';
 export const API_URL = 'https://niilz.de';
 export const API_URL_LOCAL_NET = 'https://oldstars-backend.ngrok.io';
+export const SESSION_TOKEN_HEADER_NAME = 'old-star-user-session';
+
 export enum METHOD {
   GET = 'GET',
   POST = 'POST',
