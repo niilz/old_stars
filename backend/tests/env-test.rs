3d4bda0ed87629e5077c2485bb737c24564931f4
niilz <niilz@outlook.de>
2025-07-20 21:32:59 +0200

    wrap set_var in unsafe blocks

--- backend/tests/env-test.rs
+++ backend/tests/env-test.rs
@@ -7,17 +7,26 @@ mod tests {
         // Given
         let db_user_key = "PG_USER";
         let db_user_dummy = "itseme";
-        env::set_var(db_user_key, db_user_dummy);
+        // Safety: This var is not set in multiple threads
+        unsafe {
+            env::set_var(db_user_key, db_user_dummy);
+        }
 
         let db_pwd_key = "PG_PWD";
         let db_pwd_dummy = "secret";
-        env::set_var(db_pwd_key, db_pwd_dummy);
+        // Safety: This var is not set in multiple threads
+        unsafe {
+            env::set_var(db_pwd_key, db_pwd_dummy);
+        }
 
         let db_url_key = "DATABASE_URL";
         let pg_user = env::var(db_user_key).unwrap();
         let pg_pwd = env::var(db_pwd_key).unwrap();
         let db_url_dummy = format!("postgres://{pg_user}:{pg_pwd}@service_name/db_name");
-        env::set_var(db_url_key, db_url_dummy);
+        // Safety: This var is not set in multiple threads
+        unsafe {
+            env::set_var(db_url_key, db_url_dummy);
+        }
 
         let url = env::var(db_url_key).unwrap();
         assert_eq!("postgres://itseme:secret@service_name/db_name", url);
