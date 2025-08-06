95a4bde833d851aca027a14f220f3cd84cdfbdaa
niilz <niilz@outlook.de>
2025-08-06 15:58:09 +0200

    Migrate history table to not contain fk to user-id

---
+++ backend/migrations/2025-08-06-135630_histories-no-fk/up.sql
@@ -0,0 +1,10 @@
+-- Your SQL goes here
+CREATE TABLE history (
+	history_id SERIAL PRIMARY KEY,
+	user_name VARCHAR NOT NULL,
+	timestamp TIMESTAMP NOT NULL,
+	beer_count INT NOT NULL,
+	shot_count INT NOT NULL,
+	other_count INT NOT NULL,
+	water_count INT NOT NULL
+);
