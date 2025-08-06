acd2fd36535f08165c52e01e815f0bedd543463a
niilz <niilz@outlook.de>
2025-08-06 11:31:45 +0200

    Solve remaining unclassed native elements

--- frontend/src/views/OneHistoryView.tsx
+++ frontend/src/views/OneHistoryView.tsx
@@ -1,27 +1,27 @@
-import { useContext } from 'react';
-import { Button } from '../components/button/Button';
-import { Header } from '../components/header/Header';
-import { UserList } from '../components/user-list/UserList';
-import { User } from '../model/User';
-import styles from './OneHistoryView.module.css';
-import { ViewContext } from '../context/Contexts';
-import { View } from './View';
-import { DateAndTime } from './ArchiveView';
+import { useContext } from 'react'
+import { Button } from '../components/button/Button'
+import { Header } from '../components/header/Header'
+import { UserList } from '../components/user-list/UserList'
+import { User } from '../model/User'
+import styles from './OneHistoryView.module.css'
+import { ViewContext } from '../context/Contexts'
+import { View } from './View'
+import { DateAndTime } from './ArchiveView'
 
 interface OneHistoryViewProps {
-  dateAndTime: DateAndTime;
-  users: User[];
+  dateAndTime: DateAndTime
+  users: User[]
 }
 export function OneHistoryView(props: OneHistoryViewProps) {
-  const { setActiveView } = useContext(ViewContext);
+  const { setActiveView } = useContext(ViewContext)
 
-  const { date, time } = props.dateAndTime;
+  const { date, time } = props.dateAndTime
 
   return (
     <div className={styles.OneHistory}>
       <Header showLogo={true} />
-      <h1>Historisches Ergebnis</h1>
-      <h2>{`${date} / ${time}`}</h2>
+      <h1 className={styles.H1}>Historisches Ergebnis</h1>
+      <h2 className={styles.H2}>{`${date} / ${time}`}</h2>
       <UserList users={props.users} isEditable={false} />
       <Button
         text="back"
@@ -34,5 +34,5 @@ export function OneHistoryView(props: OneHistoryViewProps) {
         callback={() => setActiveView(View.Playground)}
       />
     </div>
-  );
+  )
 }
