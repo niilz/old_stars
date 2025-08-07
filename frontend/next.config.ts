067a0b840264cb65a712d5b7cae27fd9ee771228
niilz <niilz@outlook.de>
2025-08-07 13:42:35 +0200

    Do not use jekyll in gh_pages action and add basePath to next-config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,8 +1,9 @@
 import type { NextConfig } from 'next'
- 
+
 const nextConfig: NextConfig = {
   output: 'export', // Outputs a Single-Page Application (SPA)
   distDir: 'build', // Changes the build output directory to `build`
+  basePath: '/old_stars', // gh_pages requires to define a basePath
 }
- 
+
 export default nextConfig
