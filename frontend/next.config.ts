97347d47261e648dfd41482d2bee1011ad634596
niilz <niilz@outlook.de>
2026-07-31 14:30:08 +0200

    Update frontend dependencies

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   output: 'export', // Outputs a Single-Page Application (SPA)
   distDir: 'build', // Changes the build output directory to `build`
   basePath: '/old_stars', // gh_pages requires to define a basePath
+  experimental: {
+    useTypeScriptCli: true,
+  },
 }
 
 export default nextConfig
